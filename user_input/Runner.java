90192cec5fa10567e0a5fd2cb4e0011917df1812
liranh26 <96885239+liranh26@users.noreply.github.com>
2022-04-04 10:09:35 +0300

    Add files via upload

---
+++ user_input/Runner.java
@@ -0,0 +1,57 @@
+package javabootcamp.basic.user_input;
+
+import java.util.Scanner;
+
+public class Runner {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		// ******** question 1 - calculate area & circumference for a given radius ********
+//		
+//		System.out.println("please enter a radius:");
+//		float radius = scanner.nextFloat();
+//		
+//		float pai = 3.14f;
+//		float circ = 2 * pai * radius;
+//		float area = radius * radius * pai;
+//		System.out.printf("For given redius: %.2f the circumference is: %.2f and the area is: %.2f",radius ,circ , area);
+//		
+
+		// ******** question 2 - calculating a toast price ********
+		
+//		int basicPrice = 12;
+//		System.out.println("Regualar aditions: olives, mushrumos, corn");
+//		System.out.println("please enter the amount of Regualar aditions to your toast:");
+//		int regualarAditions = scanner.nextInt();
+//		
+//		System.out.println("Regualar aditions: bulgarian cheese, extra cheese");
+//		System.out.println("please enter the amount of Special aditions to your toast:");
+//		int specialAditions = scanner.nextInt();
+//		
+//		int totalPrice = basicPrice + regualarAditions * 2 + specialAditions * 3;
+//		
+//		System.out.println("Your bill for the toast is:" + totalPrice);
+
+		
+		// ******** question 3 - calculating bill for a sofa including delivery ********
+		
+//		System.out.println("Please enter the amount of the sofa price");
+//		int sofaPrice = scanner.nextInt();
+//		
+//		System.out.println("Please enter the amount of the sofa weight in kg:");
+//		int weight = scanner.nextInt();
+//		
+//		System.out.println("Please enter the distance you live in km:");
+//		int distance = scanner.nextInt();
+//		
+//		System.out.println("Please enter your apartment floor:");
+//		int floor = scanner.nextInt();
+//		
+//		double totalPrice = sofaPrice + distance*5 + floor*weight + sofaPrice*0.1;
+//		System.out.println("The total amount of the sofa is: "+ totalPrice);
+		
+		scanner.close();
+	}
+
+}
