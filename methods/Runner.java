f4f7c5e7581d7f8eda11a221f43d12323ac20353
liranh26 <96885239+liranh26@users.noreply.github.com>
2022-04-06 17:13:34 +0300

    Add files via upload

--- methods/Runner.java
+++ methods/Runner.java
@@ -1,24 +1,133 @@
 package javabootcamp.basic.methods;
 
+import java.lang.Math;
+
 public class Runner {
 
 	public static void main(String[] args) {
+
+//		int exam1 = 92, exam2 = 94, exam3 = 97;
+
+//		printAvergaGrade(exam1, exam2, exam3);
+
+//		int maxGrade=getMax(exam1, exam2, exam3);
+//		System.out.println(maxGrade);
+
+//		System.out.println(getMax(exam1,exam2));
+//		System.out.println(getMax(exam1,exam2,exam3));
+
+//		System.out.println(10*Math.sin(Math.PI/4));
+//		System.out.println("120 degrees in radians is: " + 120*Math.PI/180);
+//		System.out.println("e^2.5 = " + Math.exp(2.5));
+
+//		int[] arr = newRandomArray(5);
+//		for(int i=0; i<arr.length;i++) {
+//			System.out.print(arr[i] + " ");
+//		}
+
+		int[] arrTest1 = {1,3,5,4,2};
+		int[] arrTest2 = {23,4,6};
 		
-		int exam1=92, exam2=94, exam3= 97;
-		float grade;
-		
-		grade = avergaGrade(exam1, exam2, exam3);
-		
+		sumArraysToLongerArray(arrTest1,arrTest2);
+		for (int i = 0; i < arrTest1.length; i++) {
+			System.out.print(arrTest1[i] + " ");
+		}
+	}
+
+	public static void printAvergaGrade(int exam1, int exam2, int exam3) {
+
+		double sum = exam1 + exam2 + exam3;
+		double grade = sum / 3;
+
 		System.out.printf("The average is: %.2f", grade);
 	}
 
-	public static float avergaGrade(int exam1,int exam2,int exam3) {
-		float grade = 0;
+	public static int getMax(int num1, int num2, int num3) {
+
+		int max = num1;
+
+		if (max < num2) {
+			max = num2;
+		}
+		if (max < num3) {
+			max = num3;
+		}
+		return max;
+	}
+
+	public static int getMax(int num1, int num2) {
+
+		int max = num1;
+
+		if (max < num2) {
+			max = num2;
+		}
+		return max;
+	}
+
+	public static int[] newRandomArray(int length) {
+		int[] arr = new int[length];
+
+		for (int i = 0; i < length; i++) {
+			arr[i] = (int) (Math.random() * 201) - 100;
+		}
+		return arr;
+	}
+
+	
+	
+	public static int[] newArraySummed(int[] longerArray, int[] shorterArray) {
+		int[] result = new int[longerArray.length];
+		for (int i = 0; i < shorterArray.length; i++) {
+			result[i] += shorterArray[i] + longerArray[i];
+		}
 		
-		grade = (exam1+exam2+exam3)/3f;
+		for (int i = shorterArray.length; i < longerArray.length ; i++) {
+			result[i] += longerArray[i];
+		}
 		
-		return grade;
+		return result;
+	}
+
+	
+	public static int[] summingToLongerArray(int[] longerArray, int[] shorterArray) {
+		
+		for (int i = 0; i < shorterArray.length; i++) {
+			longerArray[i] += shorterArray[i];
+		}
+		
+		return longerArray;
 	}
 	
 	
+	
+	public static void sumArraysToLongerArray(int[] arr1, int[] arr2) {
+		int[] longerArray = arr1, shorterArray = arr2;
+		//find the longer array between the two
+		if(arr1.length < arr2.length) {
+			longerArray = arr2; 
+			shorterArray = arr1;
+		}
+		
+//		arr1[3] = 1000;  // --> changes the original array!
+//		longerArray[2] = 1001; //--> changes the original array!
+		
+		//shallow copy & lossing the original address
+		System.out.println(longerArray);
+		longerArray = newArraySummed(longerArray, shorterArray); //--> NOT changes the original array!
+		System.out.println(longerArray);
+		
+		//shallow copy & changing the original array
+		longerArray=arr1;
+		longerArray = summingToLongerArray(longerArray, shorterArray);
+		System.out.println(longerArray);
+//		//shallow copy
+//		arr1 = newArraySummed(longerArray, shorterArray);
+		
+		
+		
+	}
+
+
+
 }
